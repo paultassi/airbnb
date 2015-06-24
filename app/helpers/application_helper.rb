6409e58dde528b0bea0803008854e87696d92026
nathan obadia <natobadia@gmail.com>
2015-06-24 15:01:32 +0200

    facebook and navbar 15h01

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def bootstrap_class_for(flash_type)
+    case flash_type.to_sym
+    when :alert   then 'alert-error'
+    when :notice  then 'alert-success'
+    end
+  end
 end
