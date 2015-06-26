029908734050b2ea1f534ab55c8725845c932744
nathan obadia <natobadia@gmail.com>
2015-06-26 10:26:19 +0200

    double check

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150625134946) do
-
+ActiveRecord::Schema.define(version: 20150625094832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +24,6 @@ ActiveRecord::Schema.define(version: 20150625134946) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "price"
   end
 
   create_table "flats", force: :cascade do |t|
